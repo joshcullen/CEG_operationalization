97be39f7ffc7fecc0a4eada0d47ba13bfdf9d04e
Josh Cullen <joshcullen10@gmail.com>
2024-11-22 08:04:58 -0800

    Added overwrite option for Heather's test GHA of raster resampling so that it wouldn't fail on push

--- data_processing/R/hw_test.R
+++ data_processing/R/hw_test.R
@@ -7,4 +7,6 @@ template=rast("data_processing/TopPredatorWatch/static/template.grd")
 file="data_acquisition/netcdfs/cmems_ncdfs/cmems_obs-sl_glo_phy-ssh_nrt_allsat-l4-duacs-0.25deg_P1D_vgosa_2024-11-14.nc"
 ras=rast(file)
 resam=terra::resample(ras,template)
-terra::writeRaster(resam,"data_processing/TopPredatorWatch/rasters/test.tiff")
+terra::writeRaster(resam,
+                   "data_processing/TopPredatorWatch/rasters/test.tiff",
+                   overwrite = TRUE)
