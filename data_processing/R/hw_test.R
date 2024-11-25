67294140e5e566a8ce295b2f8f4d5580b7ab0f78
Heather Welch <heather.welch@noaa.gov>
2024-11-25 10:52:01 -0800

    commenting out hw_test.R and hw_test.yml so that these test actions stop running each day/commit

--- data_processing/R/hw_test.R
+++ data_processing/R/hw_test.R
@@ -1,12 +1,12 @@
 ## test resample
-library(terra)
-
-setGDALconfig("GDAL_PAM_ENABLED", "FALSE")  #to prevent writing aux.xml files as well
-
-template=rast("data_processing/TopPredatorWatch/static/template.grd")
-file="data_acquisition/netcdfs/cmems_ncdfs/cmems_obs-sl_glo_phy-ssh_nrt_allsat-l4-duacs-0.25deg_P1D_vgosa_2024-11-14.nc"
-ras=rast(file)
-resam=terra::resample(ras,template)
-terra::writeRaster(resam,
-                   "data_processing/TopPredatorWatch/rasters/test.tiff",
-                   overwrite = TRUE)
+# library(terra)
+# 
+# setGDALconfig("GDAL_PAM_ENABLED", "FALSE")  #to prevent writing aux.xml files as well
+# 
+# template=rast("data_processing/TopPredatorWatch/static/template.grd")
+# file="data_acquisition/netcdfs/cmems_ncdfs/cmems_obs-sl_glo_phy-ssh_nrt_allsat-l4-duacs-0.25deg_P1D_vgosa_2024-11-14.nc"
+# ras=rast(file)
+# resam=terra::resample(ras,template)
+# terra::writeRaster(resam,
+#                    "data_processing/TopPredatorWatch/rasters/test.tiff",
+#                    overwrite = TRUE)
