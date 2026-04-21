f22d64165192aeddbfe90244a61f1c19190108cd
joshcullen <joshcullen10@gmail.com>
2026-04-21 10:17:15 -0400

    Updated test_zarr.py and test_zarr.yml to try using WIF for GCS creds during GHA

--- data_processing/R/test_zarr.py
+++ data_processing/R/test_zarr.py
@@ -2,8 +2,8 @@
 ### Test reading Zarr from GCS bucket ###
 
 import xarray as xr
-import zarr
-import gcsfs
+# import zarr
+# import gcsfs
 
 # Define where to store Zarr file in bucket (and here, I'm telling it the new "zarr_cmems" folder to create and write all subfolders)
 zarr_path = "gs://esd-climate-ecosystems-dev/zarr_cmems"
@@ -11,5 +11,6 @@ zarr_path = "gs://esd-climate-ecosystems-dev/zarr_cmems"
 # Now try reading this Zarr file stored in the bucket
 ds_cloud = xr.open_zarr(zarr_path, 
                         consolidated=False,
-                        storage_options={"token": "gcs_key.json"})  # W/ current permissions, requires creds
+                        storage_options={"token": "google_default"})
+                        # storage_options={"token": "gcs_key.json"})  # W/ current permissions, requires creds
 print(ds_cloud)
